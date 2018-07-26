7dd64424298ca17c0d3171b806234c81bc0cd5ec
Mary Shelley <mshelley@rstudio03.research.sesync.org>
2018-07-26 11:57:54 -0400

    adjusted fips vars, update census_script.R

--- census_script.R
+++ census_script.R
@@ -9,10 +9,54 @@ library(devtools)
 library(ggplot2)
 library(dplyr)
 library(tidycensus)
+library(data.table)
 
 # Put your key in the quotes and run so it doesn't have to be specified wiht each api call
 census_api_key("", install=T)
 
-x<-get_acs(geography='block group', table='B03002', year=2016, state="MD")
+# Import tables of desired variables and counties
+counties<-fread("MergedCounties.csv") %>%
+          select(STATE_FIPS, CNTY_FIPS)
 
+census_tables<-fread("CensusTables.csv", head=F)
+
+# Here is the format for requesting block group data from a year in the acs by county
+x<-get_acs(geography='block group', table='B03002', year=2016, state=36, county=27)
+
+
+for (t in 1:2)) {
+  tabn = census_tables[t,1]
+  print(tabn)
+  
+  for (c in 1:2) {
+    st=counties[c,1]
+    co=count....
+    x<-get_acs(geography='block group', table='B03002', year=2016, state=st, county=co)
+    
+  }
+
+  
+
+
+
+
+
+for (t in 1: nrow(census_tables)) {
+  tabn = census_tables[t,1]
+  print(tabn)
+  
+  for (c in 1:nrow(counties)) {
+    st=counties[c,1]
+    co=count....
+    x<-get_acs(geography='block group', table='B03002', year=2016, state=st, county=co)
+    
+  }
+  
+  
+  
+  t+1
+  
+}
+     
+ 
 
