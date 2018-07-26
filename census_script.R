f820688f8425d3c7167bd1357b0033ad70bd5e01
Mary Shelley <mshelley@rstudio03.research.sesync.org>
2018-07-26 15:40:50 -0400

    working trial loop

--- census_script.R
+++ census_script.R
@@ -21,42 +21,23 @@ counties<-fread("MergedCounties.csv") %>%
 census_tables<-fread("CensusTables.csv", head=F)
 
 # Here is the format for requesting block group data from a year in the acs by county
-x<-get_acs(geography='block group', table='B03002', year=2016, state=36, county=27)
+#x<-get_acs(geography='block group', table='B03002', year=2016, state=36, county=27)
 
+tbl <- data.frame()
 
-for (t in 1:2)) {
+for (t in 1:2) {
   tabn = census_tables[t,1]
-  print(tabn)
   
   for (c in 1:2) {
     st=counties[c,1]
-    co=count....
-    x<-get_acs(geography='block group', table='B03002', year=2016, state=st, county=co)
-    
+    co=counties[c,2]
+    x<-get_acs(geography='block group', table=tabn, year=2016, state=st, county=co)
+    tbl<-rbind(tbl,x)
+    c+1
   }
-
-  
-
-
-
-
-
-for (t in 1: nrow(census_tables)) {
-  tabn = census_tables[t,1]
-  print(tabn)
-  
-  for (c in 1:nrow(counties)) {
-    st=counties[c,1]
-    co=count....
-    x<-get_acs(geography='block group', table='B03002', year=2016, state=st, county=co)
-    
-  }
-  
-  
-  
   t+1
-  
 }
-     
- 
+  
+
+
 
