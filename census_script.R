e6b153d0fbf290686b23e49696568429542ffd4d
Mary Shelley <mshelley@rstudio03.research.sesync.org>
2018-07-26 16:54:55 -0400

    loop with spread, but vars not quite right

--- census_script.R
+++ census_script.R
@@ -7,6 +7,7 @@
 
 library(devtools)
 library(ggplot2)
+library(tidyr)
 library(dplyr)
 library(tidycensus)
 library(data.table)
@@ -20,24 +21,36 @@ counties<-fread("MergedCounties.csv") %>%
 
 census_tables<-fread("CensusTables.csv", head=F)
 
-# Here is the format for requesting block group data from a year in the acs by county
-#x<-get_acs(geography='block group', table='B03002', year=2016, state=36, county=27)
+# Get list of vars in 2015 5 yr ACS
+vars<-load_variables(2015,"acs5/profile")
 
+
+# Here is the format for requesting block group data from a year in the acs by county
+#x<-get_acs(geography='block group', variable='', year=2016, state=36, county=27)
+
+# Create blank data frame for holding api returns
 tbl <- data.frame()
 
-for (t in 1:2) {
+# Loop through the list of tables
+for (t in 1:nrow(census_tables)) {
   tabn = census_tables[t,1]
   
-  for (c in 1:2) {
+  # For each table, loop through all the counties to return blockgroup-level info and rbind to tbl
+  for (c in 1:nrow(census_tables)) {
     st=counties[c,1]
     co=counties[c,2]
-    x<-get_acs(geography='block group', table=tabn, year=2016, state=st, county=co)
+    x<-get_acs(geography='block group', table=tabn, year=2015, state=st, county=co)
     tbl<-rbind(tbl,x)
     c+1
   }
   t+1
 }
   
+# Write tbl results 
+#write.csv(tbl, "/nfs/urbanwoodlands-data/")
+
+# Spread out the eav structure
+tidytbl<-spread(tbl, key = variable, value=estimate)
 
 
 
