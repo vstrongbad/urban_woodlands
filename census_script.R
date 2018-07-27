c3237e4805489f123327485065fcf9ae74382b49
Mary Shelley <mshelley@rstudio03.research.sesync.org>
2018-07-26 21:11:01 -0400

    limit to unique counties

--- census_script.R
+++ census_script.R
@@ -15,42 +15,30 @@ library(data.table)
 # Put your key in the quotes and run so it doesn't have to be specified wiht each api call
 census_api_key("", install=T)
 
-# Import tables of desired variables and counties
+# Import tables of desired variables and counties; limit to unique counties
 counties<-fread("MergedCounties.csv") %>%
-          select(STATE_FIPS, CNTY_FIPS)
-
-census_tables<-fread("CensusTables.csv", head=F)
-
-# Get list of vars in 2015 5 yr ACS
-vars<-load_variables(2015,"acs5/profile")
-
-
-# Here is the format for requesting block group data from a year in the acs by county
-#x<-get_acs(geography='block group', variable='', year=2016, state=36, county=27)
+          select(STATE_FIPS, CNTY_FIPS) %>%
+          unique()
+census_vars <- as.character(read.csv("CensusVars.csv", head=F)$V1)
 
 # Create blank data frame for holding api returns
 tbl <- data.frame()
-
-# Loop through the list of tables
-for (t in 1:nrow(census_tables)) {
-  tabn = census_tables[t,1]
-  
-  # For each table, loop through all the counties to return blockgroup-level info and rbind to tbl
-  for (c in 1:nrow(census_tables)) {
+# Loop through all the counties to return blockgroup-level info and rbind to tbl
+for (c in 1:nrow(counties)) {
     st=counties[c,1]
     co=counties[c,2]
-    x<-get_acs(geography='block group', table=tabn, year=2015, state=st, county=co)
+    x<-get_acs(geography='block group', variables=census_vars, year=2015, state=st, county=co)
     tbl<-rbind(tbl,x)
     c+1
   }
-  t+1
-}
+
   
 # Write tbl results 
 #write.csv(tbl, "/nfs/urbanwoodlands-data/")
 
 # Spread out the eav structure
-tidytbl<-spread(tbl, key = variable, value=estimate)
+tidytbl<-select(tbl, -moe) %>%
+         spread(tbl, key = variable, value=estimate)
 
 
 
