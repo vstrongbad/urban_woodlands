ece2ac47d674eff4ce0b9a670bad9c0ed9d3aa5e
D�Amico <vdamico@rstudio03.research.sesync.org>
2018-07-27 12:16:36 -0400

    Wondering where this saves to?

--- script_to_convert_data.R
+++ script_to_convert_data.R
@@ -9,10 +9,15 @@ library(RColorBrewer)
 library(plotly)
 library(tibble)
 
-#get some sample data
+#get some sample data and convert it
 
 NPS_trees <- read_csv(file="tree_data_nps.csv")
 
 FRAME_trees<-read.csv(file="tree_data_frame_2015.csv")
 
-new<-tibble(site_long_names=(unique(FRAME_trees$site)), Unit_Code = c("ANDO","CHR1","CHR2","COVE","CHAM","CROI","CRON","CROW","CHRY","DORO","ECOL","FOLK","GLA1","GLA2","IRO1","IRO2","LAIR","LONS","LONT","MTCI","MTCR","MOTO","PARK","PEAE","PEAW","PENN","PHIL","RESE","RITT","SMIT","STRP","STRS","SUN1","SUN2","WALT","WEBB","WHI1","WHI2"))
+new<-tibble(site_long_names=(unique(FRAME_trees$site)), 
+            Unit_Code = c("ANDO","CHR1","CHR2","COVE","CHAM","CROI",
+            "CRON","CROW","CHRY","DORO","ECOL","FOLK","GLA1","GLA2",
+            "IRO1","IRO2","LAIR","LONS","LONT","MTCI","MTCR","MOTO",
+            "PARK","PEAE","PEAW","PENN","PHIL","RESE","RITT","SMIT",
+            "STRP","STRS","SUN1","SUN2","WALT","WEBB","WHI1","WHI2"))
