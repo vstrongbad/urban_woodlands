d75ad873ebee2ce388e033b516ce439f0bfc689e
Mary Shelley <mshelley@rstudio03.research.sesync.org>
2018-07-26 21:27:53 -0400

    census_script.R fully functional

--- census_script.R
+++ census_script.R
@@ -1,8 +1,5 @@
 #
-#library(raster)
-#library(gfcanalysis)
-#
-#test <-raster('/nfs/mshelley-data/hansen test.tif')
+# Script extracts census info based on input list of counties (st and county fips) and census variable names
 #
 
 library(devtools)
@@ -16,10 +13,10 @@ library(data.table)
 census_api_key("", install=T)
 
 # Import tables of desired variables and counties; limit to unique counties
-counties<-fread("MergedCounties.csv") %>%
+counties<-fread("/nfs/urbanwoodlands-data/Datasets/Data - Socioeconomic Context/Census Data/MergedCounties.csv") %>%
           select(STATE_FIPS, CNTY_FIPS) %>%
           unique()
-census_vars <- as.character(read.csv("CensusVars.csv", head=F)$V1)
+census_vars <- as.character(read.csv("/nfs/urbanwoodlands-data/Datasets/Data - Socioeconomic Context/Census Data/CensusVars.csv", head=F)$V1)
 
 # Create blank data frame for holding api returns
 tbl <- data.frame()
@@ -32,13 +29,18 @@ for (c in 1:nrow(counties)) {
     c+1
   }
 
-  
-# Write tbl results 
-#write.csv(tbl, "/nfs/urbanwoodlands-data/")
-
 # Spread out the eav structure
 tidytbl<-select(tbl, -moe) %>%
-         spread(tbl, key = variable, value=estimate)
+         spread(key = variable, value=estimate)
+
+# Check that blockgroups are unique: yes
+  #select(tidytbl, GEOID, NAME) %>%
+  #unique() %>%
+  #nrow()
+  
+# Write tidy results 
+  write.csv(tidytbl, "/nfs/urbanwoodlands-data/Datasets/Data - Socioeconomic Context/Census Data/acs2015_blkgrps_111counties.csv")
+
 
 
 
