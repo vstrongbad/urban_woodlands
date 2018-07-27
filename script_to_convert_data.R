bfba294dc9631bb427df083c2905bf45fbf110f6
D�Amico <vdamico@rstudio03.research.sesync.org>
2018-07-27 13:27:13 -0400

    Aimed script to files in nfs/urbanwoodlands-data

--- script_to_convert_data.R
+++ script_to_convert_data.R
@@ -11,11 +11,11 @@ library(tibble)
 
 #get some sample data and convert it from x to NPS JPS
 
-NPS_trees <- read_csv(file="tree_data_nps.csv")
+NPS_trees <- read.csv(file="/nfs/urbanwoodlands-data/Datasets/Data - Forest Composition and Condition/Data - Forest Plots/Data to test conversion to NPS/tree_data_nps.csv")
 
-FRAME_trees<-read.csv(file="tree_data_frame_2015.csv")
+FRAME_trees<-read.csv(file="/nfs/urbanwoodlands-data/Datasets/Data - Forest Composition and Condition/Data - Forest Plots/Data to test conversion to NPS/tree_data_frame_2015.csv")
 
-new<-tibble(site_long_names=(unique(FRAME_trees$site)), 
+plot_name_key<-tibble(site_long_names=(unique(FRAME_trees$site)), 
             Unit_Code = c("ANDO","CHR1","CHR2","COVE","CHAM","CROI",
             "CRON","CROW","CHRY","DORO","ECOL","FOLK","GLA1","GLA2",
             "IRO1","IRO2","LAIR","LONS","LONT","MTCI","MTCR","MOTO",
